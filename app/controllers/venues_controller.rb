dfbb99eec24b1f7cae70edf0fa9469da145134a6
arissketch <60331491+arissketch@users.noreply.github.com>
2024-05-08 18:34:13 +0000

    help me

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -16,7 +16,7 @@ class VenuesController < ApplicationController
   end
 
   def create
-    @venue = Venue.new
+    venue = Venue.new
     venue.address = params.fetch("query_address")
     venue.name = params.fetch("name")
     venue.neighborhood = params.fetch("neighborhood")
@@ -26,11 +26,12 @@ class VenuesController < ApplicationController
   end
   
   def update
-    the_id = params.fetch("venue_id")
-
-    @venue = Venue.where({ :id => the_id })
+    the_id = params.fetch("an_id")
+    matching_venue = Venue.where({ :id => the_id })
+    venu = matching_venue.at(0)
+    
     venue.address = params.fetch("query_address")
-    venue.name = params.fetch("Query_name")
+    venue.name = params.fetch("query_name")
     venue.neighborhood = params.fetch("query_neighborhood")
     venue.save
     
