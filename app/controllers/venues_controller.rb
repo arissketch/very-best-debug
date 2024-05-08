46d0d8623f9fcefe78cee66b01cdc21549706cea
arissketch <60331491+arissketch@users.noreply.github.com>
2024-05-08 20:21:19 +0000

    done

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,8 +1,8 @@
 class VenuesController < ApplicationController
 
   def index
-    matching_venues = venue.all
-    venues = matching_venues.order(:created_at)
+    matching_venues = Venue.all
+    @venues = matching_venues.order(:created_at)
 
     render({ :template => "venue_templates/venue_list" })
   end
@@ -18,30 +18,30 @@ class VenuesController < ApplicationController
   def create
     venue = Venue.new
     venue.address = params.fetch("query_address")
-    venue.name = params.fetch("name")
-    venue.neighborhood = params.fetch("neighborhood")
-    venue.save
-
-    redirect_to("/venues/#{venue.name}")
-  end
-  
-  def update
-    the_id = params.fetch("an_id")
-    matching_venue = Venue.where({ :id => the_id })
-    venu = matching_venue.at(0)
-    
-    venue.address = params.fetch("query_address")
     venue.name = params.fetch("query_name")
     venue.neighborhood = params.fetch("query_neighborhood")
     venue.save
-    
+
     redirect_to("/venues/#{venue.id}")
+  end
+  
+  def update
+    venue_id = params.fetch("id")
+    matching_venues = Venue.where({ :id => venue_id })
+    the_venue = matching_venues.at(0)
+    
+    the_venue.address = params.fetch("query_address")
+    the_venue.name = params.fetch("query_name")
+    the_venue.neighborhood = params.fetch("query_neighborhood")
+    the_venue.save
+    
+    redirect_to("/venues/#{the_venue.id}")
   end
 
   def destroy
-    the_id = params.fetch("venue_id")
+    the_id = params.fetch("id")
     matching_venues = Venue.where({ :id => the_id })
-    venue = matching_venues
+    venue = matching_venues.at(0)
     venue.destroy
 
     redirect_to("/venues")
