b24069ef2688a114771e24629f2712ac8a24b8ae
arissketch <60331491+arissketch@users.noreply.github.com>
2024-05-08 17:57:49 +0000

    first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     matching_users = User.all
     @users = matching_users.order(:created_at)
 
-    render({ :template => "users_templates/all_users"})
+    render({ :template => "user_templates/all_users"})
   end
   
   def show
@@ -32,5 +32,8 @@ class UsersController < ApplicationController
     the_user.save
     redirect_to("/users/#{user.username}")
   end
+
+
+
 
 end
